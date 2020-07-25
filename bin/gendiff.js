b837b0ceac793bf962ed0100ce344a31e2f2b156
Aleskey Emelianovich <aemelianovich@yandex.ru>
2020-07-25 18:23:49 -0500

    2step. Added -f option and filepath arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,10 +4,23 @@ import commander from 'commander';
 
 const gendiff = new commander.Command();
 
+let filepath1Value;
+let filepath2Value;
+
 gendiff
-  .version('001', '-V, --version', 'output the version number')
-  .description('Compares two configuration files and shows a difference.');
+  .version('0.1.0', '-V, --version', 'output the version number')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format');
+
+gendiff
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    filepath1Value = filepath1;
+    filepath2Value = filepath2;
+  });
 
 gendiff.parse(process.argv);
 
-if (!gendiff.args.length) gendiff.help();
+if (gendiff.format) console.log(`- ${gendiff.format}`);
+console.log(`filepath1: ${filepath1Value}`);
+console.log(`filepath2: ${filepath2Value}`);
